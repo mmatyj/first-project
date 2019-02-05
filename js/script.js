47126a25c034ebe48500bdd4c03ee3c2e166f549
Malgorzata Matyjanka <m.matyjanka@gmail.com>
2019-02-05 22:47:13 +0000

    Add quit-modal

--- js/script.js
+++ js/script.js
@@ -1,8 +1,39 @@
+// HAMBURGER
 document.addEventListener('DOMContentLoaded', function () {
     document.querySelector('.js--toggle-menu').addEventListener('click', function (e) {
         e.preventDefault();
 
-
         document.querySelector('.sidebar').classList.toggle('open');
     })
+})
+
+
+// CLOSING MODALS
+function closeModal() {
+    document.getElementById('overlay').classList.remove('show')
+}
+
+document.querySelectorAll('#overlay .js--close-modal').forEach(function (btn) {
+    btn.addEventListener('click', function (e) {
+        e.preventDefault()
+        closeModal()
+    })
+})
+
+document.querySelector('#overlay').addEventListener('click', function (e) {
+    if (e.target === this) {
+        closeModal()
+    }
+})
+
+document.addEventListener('keyup', function (e) {
+    if (e.keyCode === 27) {
+        closeModal()
+    }
+})
+
+// OPENING MODAL QUIT
+
+document.querySelector('.quit').addEventListener('click', function () {
+    document.querySelector('#overlay').classList.add('show')
 })
